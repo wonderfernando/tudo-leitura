fd2f41f542ffcec01be943b46747d289509ec1a7
Fernando Silva <fernandowonder123@gmail.com>
2024-02-08 19:49:08 +0100

    more changes

---
+++ js/animation-reveal.js
@@ -0,0 +1,32 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal(".efeito-titulo-topo", {
+    duration:2000,
+    distance: "90px",
+    reset:true
+})
+ 
+revelar.reveal(".efeito-tudo", {
+    duration:2000,
+    distance: "90px"
+})
+revelar.reveal(".efeito-fundador", {
+    duration:2000,
+    distance: "90px"
+})
+revelar.reveal(".efeito-caue", {
+    duration:2000,
+    distance: "90px",
+    delay:400
+})
+revelar.reveal(".efeito-coriolano", {
+    duration:2000,
+    distance: "90px",
+    delay:400
+})
+revelar.reveal(".efeito-video", {
+    duration:2000,
+    distance: "90px",
+    delay:800
+})
+
